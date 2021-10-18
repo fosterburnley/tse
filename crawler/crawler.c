d2d198a8e6e982253c690d106cff0024b21168c7
Foster Burnley <Foster.Burnley.22@dartmouth.edu>
2021-10-18 11:07:22 -0400

    logic for step 3 of mod 4 done. having issue with print_page func in crawler.c when compiling

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -2,9 +2,18 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "../utils/webpage.h"
-//#include "../utils/queue.h"
+#include "../utils/queue.h"
 //#include "../utils/hash.h"
 //#include <queue.h>
+
+void print_page(void * page_p){
+  if (page_p == NULL){
+    printf("page not found\n");
+    return;
+  }
+  webpage_t * webpage_p = (webpage_t *) page_p;
+  printf("%s\n", webpage_p -> url);
+}
 
 
 int main(void){
@@ -12,6 +21,8 @@ int main(void){
   // print "hello" used in step 1 of lab...
   //printf("hello\n");
 
+  queue_t *page_queue;
+  
   // 1. Create a new webpage at depth 0, with the seed URL: https://thayer.github.io/engs50/
   webpage_t *page = webpage_new("https://thayer.github.io/engs50/", 0, NULL);
     
@@ -19,15 +30,22 @@ int main(void){
   // 3. Check that the fetch succeeded and if not exit with EXIT_FAILURE
   if (webpage_fetch(page)){
 
-    // 4. Scan the fetched html... 
-    //char * html = webpage_getHTML(page);  ... ?? Don't think we need this line ??
+    // 4. Scan the fetched html...
     int pos = 0;
     char * result;
+    page_queue = qopen();  // queue to hold the newly created webpages for internal URLs
+    
     while ((pos = webpage_getNextURL(page, pos, &result)) > 0){
       
       // ...and print all the URL's it contains, one per line, with an indicator to say it is internal or external
       if (IsInternalURL(result)){
-	printf("Internal URL : %s\n", result); 
+	printf("Internal URL : %s\n", result);
+
+	// Step 3 of Mod 4. Queue of Webpages. Need to make webapge types for the internal URLs and put into the queue
+	webpage_t * new_page = webpage_new(result, 0, NULL);  // ?? might initialize with depth as "pos" ??
+	qput(page_queue, (void*) new_page);
+	// STILL NEED TO FREE new_page **
+	
       }else{
 	printf("External URL : %s\n", result); 
       }
@@ -35,9 +53,14 @@ int main(void){
     }
     
   }else{
+    free(page);
     exit(EXIT_FAILURE); 
   }
-    
+  
+  // need to print the queue then close it
+  qapply(page_queue, print_page);
+  qclose(page_queue); 
+  
   // 5. De-allocate the webpage and terminate it with EXIT_SUCCESS
   free(page);
   return EXIT_SUCCESS;
