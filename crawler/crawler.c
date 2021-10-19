e40d0bd8b423d5dc2492497e237e7b6d6feacdf0
Foster Burnley <Foster.Burnley.22@dartmouth.edu>
2021-10-18 20:59:25 -0400

    step 4 works, trying to solve memory leaks

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -13,23 +13,18 @@ void print_page(void * page_p){
     return;
   }
   webpage_t* webpage_p = (webpage_t *) page_p;
-
-	printf("%s\n", webpage_getURL(webpage_p));
+  printf("%s\n", webpage_getURL(webpage_p));
 }
 
-bool searchfn1(void* element, const void* keyp){
-		printf("in search function\n");
-	//	webpage_t* webpage_p;
-	//webpage_p = (webpage_t*) webpage;
-	//char *key = (char *) keyp;
-	if (element == keyp){
-		printf("found\n");
-		return true;
-	}
-	else{
-		printf("not found\n");
-		return false; 
-	}
+static bool searchfn1(void* element, const void* keyp){
+  webpage_t * webpage_p = (webpage_t *) element;
+  char * key = (char *) keyp;
+ 
+  if (strcmp(webpage_getURL(webpage_p),key) == 0){
+    return true;
+  }else{
+    return false; 
+  }
 }
 
 
@@ -56,46 +51,43 @@ int main(void){
       
       // ...and print all the URL's it contains, one per line, with an indicator to say it is internal or external
       if (IsInternalURL(result)){
-				//	printf("Internal URL : %s\n", result);
+	// Step 3 of Mod 4. Queue of Webpages. Need to make webapge types for the internal URLs and put into the queue
+	webpage_t * new_page = webpage_new(result, 0, NULL);  // ?? might initialize with depth as "pos" ??
+	
+	// Step 4 of Mod 4. Hash of webpages. 
+	// check if webpage is in the hashtable
+	// if not, then put it in hashtable and put it in queue
+	// if yes, then do not put in hashtable or queue and move on
+	webpage_t *found = (webpage_t*) hsearch(page_hash, searchfn1, webpage_getURL(new_page), (int) strlen(webpage_getURL(new_page)));
 
-				// Step 3 of Mod 4. Queue of Webpages. Need to make webapge types for the internal URLs and put into the queue
-				webpage_t * new_page = webpage_new(result, 0, NULL);  // ?? might initialize with depth as "pos" ??
+	//printf("current webpage in question: %s\n", webpage_getURL(new_page));
+	//printf("URL length: %d\n", (int) strlen(webpage_getURL(new_page)));
+	printf("webpage after finding in hash table: %s\n", webpage_getURL(found));
 
-				// step 4 of Mod 4- hash of webpages 
-				//check if webpage is in the hashtable
-				// if not, then put it in hashtable and put it in queue
-				// if yes, then do not put in hashtable or queue and move on
-				webpage_t *found = (webpage_t*) hsearch(page_hash, searchfn1, webpage_getURL(new_page), (int) strlen(webpage_getURL(new_page)));
-
-				printf("current webpage in question: %s\n", webpage_getURL(new_page));
-				printf("URL length: %d\n", (int) strlen(webpage_getURL(new_page)));
-				printf("webpage after finding in hash table: %s\n", webpage_getURL(found));
-				if (found == NULL)
-				 {
-				hput(page_hash, (void*) new_page, webpage_getURL(new_page), (int) strlen(webpage_getURL(new_page)));
-				qput(page_queue, (void*) new_page);
-				}
-				// STILL NEED TO FREE new_page **
-      }
-			else{
-				printf("External URL : %s\n", result); 
+	if (found == NULL){
+	    hput(page_hash, (void*) new_page, webpage_getURL(new_page), (int) strlen(webpage_getURL(new_page)));
+	    qput(page_queue, (void*) new_page);
+	}
+      }else{
+	printf("\nExternal URL : %s\n", result); 
       }
       free(result);
     }
     
-  }
- else{
+  }else{
     free(page);
     exit(EXIT_FAILURE); 
   }
   
-  // need to print the queue then close it
-	printf("internal hash of webpages...\n");
-	happly(page_hash, print_page);
-  printf("internal queue of webpages...\n");
-	qapply(page_queue, print_page);
-	hclose(page_hash);
+  // need to print the hash table and queue...
+  printf("\ninternal hash of webpages...\n");
+  happly(page_hash, print_page);
+  printf("\ninternal queue of webpages...\n");
+  qapply(page_queue, print_page);
+  // ... then close them.
+  hclose(page_hash);
   qclose(page_queue);
+
   // 5. De-allocate the webpage and terminate it with EXIT_SUCCESS
   free(page);
   return EXIT_SUCCESS;
